5aed5701e312fea61bd29338085600136acb027a
Luli <lulimanucians198.com>
2025-04-21 18:34:09 +0700

    Test pre-commit hook1

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
-import "./App.css";
-import { Button } from "@/components/ui/button";
+import { useState } from 'react';
+import './App.css';
+// import { Button } from '@/components/ui/button';
 
 function App() {
-
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-svh">
