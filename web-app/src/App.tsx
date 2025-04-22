0891e68908f4b31ce4624596b873d586acf3e3c2
Luli <lulimanucians198.com>
2025-04-22 14:23:23 +0700

    feat: login by google

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,13 +1,40 @@
+import { useState } from 'react';
 import './App.css';
 import { Button } from '@/components/ui/button';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import { auth, provider } from './firebaseConfig';
+import { signInWithPopup } from 'firebase/auth';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const loginWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      setUser(result.user);
+    } catch (error) {
+      console.error('Error during Google sign-in:', error);
+      alert('Error logging in with Google. Please try again.');
+    }
+  };
   return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-svh">
-        <Button>Click me</Button>
-      </div>
-    </>
+    <Router>
+      {!user ? (
+        <div className="flex flex-col items-center justify-center min-h-svh">
+          <Button onClick={loginWithGoogle}>Đăng Nhập Bằng GOOGLE</Button>
+        </div>
+      ) : (
+        <Navigate to="/home" />
+      )}
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ web-app/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Đây là Trang Chính sau khi bạn đăng nhập!</h1>
+    </div>
+  );
+};
+export default Home;
