5cecacdafee0c9e3b5ffc6d6b41195dc30ebde14
Luli <lulimanucians198.com>
2025-04-26 08:27:12 +0700

    fix use env, not hard config

--- web-app/src/firebaseConfig.ts
+++ web-app/src/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAaEhAgflkHfZUeD63nEKOHFc71if1ql-4',
-  authDomain: 'apponi-6c0fc.firebaseapp.com',
-  projectId: 'apponi-6c0fc',
-  storageBucket: 'apponi-6c0fc.firebasestorage.app',
-  messagingSenderId: '573703309847',
-  appId: '1:573703309847:web:97a94e060a80f8e30caa52',
-  measurementId: 'G-BS8D0GMVFZ',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
