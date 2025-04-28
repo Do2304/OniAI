446baeac31dd7beb820948ea29ddf5b2d8f8fa4b
Luli <lulimanucians198.com>
2025-04-28 11:27:25 +0700

    feat: apiPath structure for API paths in Axios service

--- web-app/src/api/apiServices.ts
+++ web-app/src/api/apiServices.ts
@@ -1,11 +1,16 @@
 import axiosInstance from '@/config/axiosConfig';
 
+const apiPath = {
+  getUser: '/users/getUser',
+  login: '/users/login',
+};
+
 export const getUserInfo = async () => {
-  const response = await axiosInstance.get('/users/getUser');
+  const response = await axiosInstance.get(apiPath.getUser);
   return response.data;
 };
 
 export const loginUser = async (email, name) => {
-  const response = await axiosInstance.post('/users/login', { email, name });
+  const response = await axiosInstance.post(apiPath.login, { email, name });
   return response.data;
 };
